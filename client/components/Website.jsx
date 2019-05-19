705531061656bdb6fa8e9d30462c5b2a9781e84d
rafeautie <rautie12@gmail.com>
2019-05-18 19:01:54 -0700

    Implement Website component

---
+++ client/components/Website.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  letter-spacing: .013em;
+  display: flex;
+  justify-content: flex-start;
+  color: #101820;
+  min-width: 380px;
+
+  &:hover {
+    color: #b70038;
+  }
+`;
+
+const Icon = styled.img`
+  padding-right: 24px;
+  padding-left: 1px;
+  width: 24px;
+`;
+
+const WebsiteA = styled.a`
+  text-decoration: none;
+  color: #101820;
+`;
+
+const WebsiteContainer = styled.span`
+  margin-top: 1px;
+`;
+
+const Website = ({ data }) => {
+  if (data) {
+    return (
+      <Container>
+        <span>
+          <Icon src="./icons/website-icon.jpg" />
+        </span>
+        <WebsiteContainer>
+          <WebsiteA href={`${data.website}`}>{data.website}</WebsiteA>
+        </WebsiteContainer>
+      </Container>
+    );
+  }
+
+  return '';
+};
+
+export default Website;
