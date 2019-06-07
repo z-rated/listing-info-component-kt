89260881bf517d91ac1de11f1ebb4619289bfb15
Kenneth Trott <kentrottjr@gmail.com>
2019-06-06 19:37:32 -0700

    Update data generation script

--- db/dataGeneration.js
+++ db/dataGeneration.js
@@ -18,47 +18,92 @@ const {
 // const out = fs.createWriteStream('input.txt.gz');
 // inp.pipe(gzip).pipe(out);
 
-const writeStream = fs.createWriteStream('./db/data-doc.csv');
+const flatDataGenerator = () => {
+  const writeStream = fs.createWriteStream('./db/flat-data-doc.csv');
 
-const header = 'id,name,address,coordinates,phoneNumber,website,monHoursOpen,monHoursClose,tuesHoursOpen,tuesHoursClose,wedsHoursOpen,wedsHoursClose,thursHoursOpen,thursHoursClose,friHoursOpen,friHoursClose,satHoursOpen,satHoursClose,sunHoursOpen,sunHoursClose' + '\n';
+  const header = 'id,name,address,coordinates,phoneNumber,website,monHoursOpen,monHoursClose,tuesHoursOpen,tuesHoursClose,wedsHoursOpen,wedsHoursClose,thursHoursOpen,thursHoursClose,friHoursOpen,friHoursClose,satHoursOpen,satHoursClose,sunHoursOpen,sunHoursClose' + '\n';
 
-writeStream.write(header);
-for (let i = 1; i < 10000001; i += 1) {
-  const n = Math.floor((Math.random() * 100) + 0);
-  const restaurantName = name[n];
-  const address = addresses[n];
-  const coordinates = coords[n];
-  const phoneNumber = phoneNumbers[n];
-  const website = urls[n];
-  const monHoursOpen = hours[n].Monday.open;
-  const monHoursClose = hours[n].Monday.close;
-  const tuesHoursOpen = hours[n].Tuesday.open;
-  const tuesHoursClose = hours[n].Tuesday.close;
-  const wedsHoursOpen = hours[n].Wednesday.open;
-  const wedsHoursClose = hours[n].Wednesday.close;
-  const thursHoursOpen = hours[n].Thursday.open;
-  const thursHoursClose = hours[n].Thursday.close;
-  const friHoursOpen = hours[n].Friday.open;
-  const friHoursClose = hours[n].Friday.close;
-  const satHoursOpen = hours[n].Saturday.open;
-  const satHoursClose = hours[n].Saturday.close;
-  const sunHoursOpen = hours[n].Sunday.open;
-  const sunHoursClose = hours[n].Sunday.close;
+  writeStream.write(header);
+  for (let i = 1; i < 10000001; i += 1) {
+    const n = Math.floor((Math.random() * 100) + 0);
+    const restaurantName = name[n];
+    const address = addresses[n];
+    const coordinates = coords[n];
+    const phoneNumber = phoneNumbers[n];
+    const website = urls[n];
+    const monHoursOpen = hours[n].Monday.open;
+    const monHoursClose = hours[n].Monday.close;
+    const tuesHoursOpen = hours[n].Tuesday.open;
+    const tuesHoursClose = hours[n].Tuesday.close;
+    const wedsHoursOpen = hours[n].Wednesday.open;
+    const wedsHoursClose = hours[n].Wednesday.close;
+    const thursHoursOpen = hours[n].Thursday.open;
+    const thursHoursClose = hours[n].Thursday.close;
+    const friHoursOpen = hours[n].Friday.open;
+    const friHoursClose = hours[n].Friday.close;
+    const satHoursOpen = hours[n].Saturday.open;
+    const satHoursClose = hours[n].Saturday.close;
+    const sunHoursOpen = hours[n].Sunday.open;
+    const sunHoursClose = hours[n].Sunday.close;
 
-  const row = `${i},${restaurantName},${address},${coordinates},${phoneNumber},${website},${monHoursOpen},${monHoursClose},${tuesHoursOpen},${tuesHoursClose},${wedsHoursOpen},${wedsHoursClose},${thursHoursOpen},${thursHoursClose},${friHoursOpen},${friHoursClose},${satHoursOpen},${satHoursClose},${sunHoursOpen},${sunHoursClose}\n`;
+    const row = `${i},${restaurantName},${address},${coordinates},${phoneNumber},${website},${monHoursOpen},${monHoursClose},${tuesHoursOpen},${tuesHoursClose},${wedsHoursOpen},${wedsHoursClose},${thursHoursOpen},${thursHoursClose},${friHoursOpen},${friHoursClose},${satHoursOpen},${satHoursClose},${sunHoursOpen},${sunHoursClose}\n`;
 
-  if (i % 1000000 === 0) {
-    console.log(i);
+    if (i % 1000000 === 0) {
+      console.log(i);
+    }
+
+    writeStream.write(row);
   }
+  writeStream.end();
+  writeStream.on('finish', () => {
+    console.log('Write success.');
+    console.log(v8.getHeapStatistics());
+  });
+  writeStream.on('error', (err) => {
+    console.log(err.stack);
+    console.log(v8.getHeapStatistics());
+  });
+};
 
-  writeStream.write(row);
-}
-writeStream.end();
-writeStream.on('finish', () => {
-  console.log('Write success.');
-  console.log(v8.getHeapStatistics());
-});
-writeStream.on('error', (err) => {
-  console.log(err.stack);
-  console.log(v8.getHeapStatistics());
-});
+const nestedDataGenerator = () => {
+  const writeStream = fs.createWriteStream('./db/nested-data-doc.csv');
+
+  const header = 'id|name|address|coordinates|phoneNumber|website|monHours|tuesHours|wedsHours|thursHours|friHours|satHours|sunHours' + '\n';
+
+  writeStream.write(header);
+  for (let i = 1; i < 10000001; i += 1) {
+    const n = Math.floor((Math.random() * 100) + 0);
+    const restaurantName = name[n];
+    const address = addresses[n];
+    const coordinates = coords[n];
+    const phoneNumber = phoneNumbers[n];
+    const website = urls[n];
+    const monHours = [hours[n].Monday];
+    const tuesHours = [hours[n].Tuesday];
+    const wedsHours = [hours[n].Wednesday];
+    const thursHours = [hours[n].Thursday];
+    const friHours = [hours[n].Friday];
+    const satHours = [hours[n].Saturday];
+    const sunHours = [hours[n].Sunday];
+
+    const row = `${i}|${restaurantName}|${address}|${coordinates}|${phoneNumber}|${website}|${JSON.stringify(monHours)}|${JSON.stringify(tuesHours)}|${JSON.stringify(wedsHours)}|${JSON.stringify(thursHours)}|${JSON.stringify(friHours)}|${JSON.stringify(satHours)}|${JSON.stringify(sunHours)}\n`;
+
+    if (i % 1000000 === 0) {
+      console.log(i);
+    }
+
+    writeStream.write(row);
+  }
+  writeStream.end();
+  writeStream.on('finish', () => {
+    console.log('Write success.');
+    console.log(v8.getHeapStatistics());
+  });
+  writeStream.on('error', (err) => {
+    console.log(err.stack);
+    console.log(v8.getHeapStatistics());
+  });
+};
+
+// flatDataGenerator();
+nestedDataGenerator();
